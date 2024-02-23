967aaf7c8b3aed302ef54169166695aa109bce91
laska-adief <laskaadief.la@gmail.com>
2024-02-23 23:42:28 +0700

    [Paypal Clone] create type BorderRadiusProp and WidthProp

---
+++ src/types/type-utility.ts
@@ -0,0 +1,12 @@
+export type BorderRadiusProp =
+  | "rounded-none"
+  | "rounded-sm"
+  | "rounded"
+  | "rounded-md"
+  | "rounded-lg"
+  | "rounded-xl"
+  | "rounded-2xl"
+  | "rounded-3xl"
+  | "rounded-full";
+
+export type WidthProp = "w-full" | "w-fit";

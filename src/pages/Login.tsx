c78a49c6c2139c78de316640b51c9078e31034d5
laska-adief <laskaadief.la@gmail.com>
2024-02-24 15:38:18 +0700

    [Paypal Clone] create onChangeEmail function in Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,11 @@ const Login = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
+  const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const valueEmail = event.target?.value;
+    setEmail(valueEmail);
+  };
+
   return <></>;
 };
 export default Login;

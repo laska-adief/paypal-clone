2cdf7f2dcab87bdae4b38ea785db83055e00bee9
laska-adief <laskaadief.la@gmail.com>
2024-02-24 15:26:46 +0700

    [Paypal Clone] add prop value in Input component

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,15 +1,17 @@
 interface InputProps {
   type: "text" | "email" | "password";
   placeholder: string;
+  value: string;
 }
 
-const Input: React.FC<InputProps> = ({ type, placeholder }) => {
+const Input: React.FC<InputProps> = ({ type, placeholder, value }) => {
   return (
     <>
       <input
         type={type}
-        className="w-full p-3 border border-light-gray rounded focus:outline-dodger-blue focus:shadow-sm focus:shadow-dodger-blue"
+        value={value}
         placeholder={placeholder}
+        className="w-full p-3 border border-light-gray rounded focus:outline-dodger-blue focus:shadow-sm focus:shadow-dodger-blue"
       />
     </>
   );

7573eaba8f7f3228ebd911134727df5f34e8c3a3
laska-adief <laskaadief.la@gmail.com>
2024-02-24 15:37:52 +0700

    [Paypal Clone] create state for email and password in Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
   return <></>;
 };
 export default Login;

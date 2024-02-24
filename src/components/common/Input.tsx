1751e27929806a92b8c33e96ff74e8a029c49339
laska-adief <laskaadief.la@gmail.com>
2024-02-24 15:27:29 +0700

    [Paypal Clone] add prop onChange in Input component

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -2,15 +2,17 @@ interface InputProps {
   type: "text" | "email" | "password";
   placeholder: string;
   value: string;
+  onChange: () => void;
 }
 
-const Input: React.FC<InputProps> = ({ type, placeholder, value }) => {
+const Input: React.FC<InputProps> = ({ type, placeholder, value, onChange }) => {
   return (
     <>
       <input
         type={type}
         value={value}
         placeholder={placeholder}
+        onChange={onChange}
         className="w-full p-3 border border-light-gray rounded focus:outline-dodger-blue focus:shadow-sm focus:shadow-dodger-blue"
       />
     </>

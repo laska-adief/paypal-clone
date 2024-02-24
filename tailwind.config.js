a5a598f75d82db141c5d74f54575890c16954073
laska-adief <laskaadief.la@gmail.com>
2024-02-24 17:04:11 +0700

    [Paypal Clone] add color, padding, and shadow in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,16 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#0544b5",
-        "dodger-blue": "#097ff5",
+        primary: "#142c8e",
+        "dodger-blue": "#1040c1",
         "light-gray": "#9da4a6",
+        "muted-gray": "#eaeced",
+      },
+      padding: {
+        form: "30px 30px 50px",
+      },
+      boxShadow: {
+        "input-focus": "inset 0 0 0 .125rem #1040c1,0 0 0 .375rem rgba(16,114,235,.16),inset 0 0 0 1px #031954,0 0 0 1px #1040c1",
       },
     },
   },

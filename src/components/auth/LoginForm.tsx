8d431c90c3f7e34083e4820bc7ac4f3c9600d1b5
laska-adief <laskaadief.la@gmail.com>
2024-02-24 17:06:27 +0700

    [Paypal Clone] add styling in LoginForm component

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,5 +1,6 @@
 import Button from "../common/Button";
 import Input from "../common/Input";
+import PaypalLogo from "./../../assets/images/paypal-logo.svg";
 
 interface LoginForm {
   email: string;
@@ -12,13 +13,20 @@ interface LoginForm {
 const LoginForm: React.FC<LoginForm> = ({ email, password, onChangeEmail, onChangePassword, onSubmit }) => {
   return (
     <>
-      <form onSubmit={onSubmit}>
-        <Input type="email" placeholder="Email" value={email} onChange={onChangeEmail} />
-        <Input type="password" placeholder="Enter your password" value={password} onChange={onChangePassword} />
-        <Button type="submit" variant="primary" width="w-full" borderRadius="rounded-full">
-          Log In
-        </Button>
-      </form>
+      <div className="grid place-items-center min-h-screen">
+        <div className="w-[460px] mx-auto p-form border border-muted-gray rounded-xl">
+          <div className="max-w-[40px] mx-auto mb-10">
+            <img src={PaypalLogo} alt="Paypal Logo" />
+          </div>
+          <form onSubmit={onSubmit} className="flex flex-col items-center gap-4">
+            <Input type="email" placeholder="Email" value={email} onChange={onChangeEmail} />
+            <Input type="password" placeholder="Enter your password" value={password} onChange={onChangePassword} />
+            <Button type="submit" variant="primary" width="w-full" borderRadius="rounded-full">
+              Log In
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };

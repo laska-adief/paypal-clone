ab44ca905d5b7c40da316b3c3f2c5037f1156de4
laska-adief <laskaadief.la@gmail.com>
2024-02-25 17:00:39 +0700

    [Paypal Clone] add text forgot password in LoginForm

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,5 +1,6 @@
 import Button from "../common/Button";
 import Input from "../common/Input";
+import Text from "../common/Text";
 import PaypalLogo from "./../../assets/images/paypal-logo.svg";
 
 interface LoginForm {
@@ -18,9 +19,12 @@ const LoginForm: React.FC<LoginForm> = ({ email, password, onChangeEmail, onChan
           <div className="max-w-[40px] mx-auto mb-10">
             <img src={PaypalLogo} alt="Paypal Logo" />
           </div>
-          <form onSubmit={onSubmit} className="flex flex-col items-center gap-4">
+          <form onSubmit={onSubmit} className="flex flex-col gap-4 text-lg">
             <Input type="email" placeholder="Email" value={email} onChange={onChangeEmail} />
             <Input type="password" placeholder="Enter your password" value={password} onChange={onChangePassword} />
+            <Text isLink={true} href="/forgot-password" size="text-lg" weight="font-bold" customClass="mb-1">
+              Forgot Password?
+            </Text>
             <Button type="submit" variant="primary" width="w-full" borderRadius="rounded-full">
               Log In
             </Button>

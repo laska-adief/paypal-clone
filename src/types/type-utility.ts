c0f7cd863b750bbc598bd5f5ee7441534733f8f0
laska-adief <laskaadief.la@gmail.com>
2024-02-25 16:40:06 +0700

    [Paypal Clone] add type FontSizeProp

--- src/types/type-utility.ts
+++ src/types/type-utility.ts
@@ -10,3 +10,18 @@ export type BorderRadiusProp =
   | "rounded-full";
 
 export type WidthProp = "w-full" | "w-fit";
+
+export type FontSizeProp =
+  | "text-xs"
+  | "text-sm"
+  | "text-base"
+  | "text-lg"
+  | "text-xl"
+  | "text-2xl"
+  | "text-3xl"
+  | "text-4xl"
+  | "text-5xl"
+  | "text-6xl"
+  | "text-7xl"
+  | "text-8xl"
+  | "text-9xl";

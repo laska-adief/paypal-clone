60f6fdc21a41ab5c756e76907f45927d457f1167
laska-adief <laskaadief.la@gmail.com>
2024-02-25 20:17:07 +0700

    [Paypal Clone] add property size, customClass, and method onMouseDown in Button component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,22 +1,30 @@
-import { BorderRadiusProp, WidthProp } from "../../types/type-utility";
+import { BorderRadiusProp, FontSizeProp, WidthProp } from "../../types/type-utility";
 
 interface ButtonProps {
   type: "submit" | "button";
-  variant: "primary" | "secondary";
+  variant: "primary" | "secondary" | "text";
+  size?: FontSizeProp;
   borderRadius?: BorderRadiusProp;
   width?: WidthProp;
+  customClass?: string;
   onClick?: () => void;
+  onMouseDown?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   children: React.ReactNode;
 }
 
 const defaultProps: Partial<ButtonProps> = {
   width: "w-fit",
   borderRadius: "rounded",
+  size: "text-base",
 };
 
-const Button: React.FC<ButtonProps> = ({ type, variant, borderRadius, width, onClick, children }) => {
+const Button: React.FC<ButtonProps> = ({ type, variant, size, borderRadius, width, customClass, onClick, onMouseDown, children }) => {
   return (
-    <button type={type} className={`button-${variant} ${borderRadius} ${width}`} onClick={onClick}>
+    <button
+      type={type}
+      className={`button-${variant} ${borderRadius} ${width} ${size} ${customClass}`}
+      onClick={onClick}
+      onMouseDown={onMouseDown}>
       {children}
     </button>
   );

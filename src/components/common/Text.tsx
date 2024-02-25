ef3ba4b78a4c14f1fe234e2cb62ef2801f856d9f
laska-adief <laskaadief.la@gmail.com>
2024-02-25 17:00:19 +0700

    [Paypal Clone] create Text component

---
+++ src/components/common/Text.tsx
@@ -0,0 +1,28 @@
+import { FontSizeProp, FontWeightProp } from "../../types/type-utility";
+
+interface TextProps {
+  size?: FontSizeProp;
+  weight?: FontWeightProp;
+  isLink?: boolean;
+  href?: string;
+  customClass?: string;
+  children: React.ReactNode;
+}
+
+const defaultProps: Partial<TextProps> = {
+  size: "text-base",
+  weight: "font-normal",
+};
+
+const Text: React.FC<TextProps> = ({ size, weight, isLink, href, customClass, children }) => {
+  return isLink ? (
+    <a href={href} className={`${size} ${weight} ${customClass} text-link`}>
+      {children}
+    </a>
+  ) : (
+    <p className={`${size} ${weight} ${customClass}`}>{children}</p>
+  );
+};
+
+Text.defaultProps = defaultProps;
+export default Text;

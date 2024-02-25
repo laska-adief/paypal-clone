83e27b95d607cae3fce3db9e06fcfb42638d86f0
laska-adief <laskaadief.la@gmail.com>
2024-02-25 20:18:08 +0700

    [Paypal Clone] add functionality hide and show password in Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,8 @@ import LoginForm from "../components/auth/LoginForm";
 const Login = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [isFocusPassword, setIsFocusPassword] = useState<boolean>(false);
+  const [isShowPassword, setIsShowPassword] = useState<boolean>(false);
 
   const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
     const valueEmail = event.target?.value;
@@ -15,13 +17,42 @@ const Login = () => {
     setPassword(valuePassword);
   };
 
+  const onShowHidePassword = (event: React.MouseEvent<HTMLButtonElement>, value: boolean) => {
+    if (event.target === event.currentTarget) {
+      event.preventDefault();
+      setIsFocusPassword(true);
+      setIsShowPassword(value);
+    }
+  };
+
+  const onFocusInputPassword = (event: React.FocusEvent<HTMLInputElement>) => {
+    const isFocus = document.activeElement === event.target;
+    setIsFocusPassword(isFocus);
+  };
+
+  const onBlurInputPassword = (event: React.FocusEvent<HTMLInputElement>) => {
+    const isFocus = document.activeElement === event.target;
+    setIsFocusPassword(isFocus);
+  };
+
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   };
 
   return (
     <>
-      <LoginForm email={email} password={password} onChangeEmail={onChangeEmail} onChangePassword={onChangePassword} onSubmit={onSubmit} />
+      <LoginForm
+        email={email}
+        password={password}
+        isShowPassword={isShowPassword}
+        isFocusPassword={isFocusPassword}
+        onChangeEmail={onChangeEmail}
+        onChangePassword={onChangePassword}
+        onShowHidePassword={onShowHidePassword}
+        onFocusInputPassword={onFocusInputPassword}
+        onBlurInputPassword={onBlurInputPassword}
+        onSubmit={onSubmit}
+      />
     </>
   );
 };

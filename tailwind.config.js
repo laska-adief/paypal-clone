3b58f5c21a4773bc0f614f92ce07f4c2b1b711bd
laska-adief <laskaadief.la@gmail.com>
2024-02-25 16:59:55 +0700

    [Paypal Clone] change padding form value and add color link in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,10 @@ export default {
         "dodger-blue": "#1040c1",
         "light-gray": "#9da4a6",
         "muted-gray": "#eaeced",
+        link: "#1072eb",
       },
       padding: {
-        form: "30px 30px 50px",
+        form: "30px 40px 50px",
       },
       boxShadow: {
         "input-focus": "inset 0 0 0 .125rem #1040c1,0 0 0 .375rem rgba(16,114,235,.16),inset 0 0 0 1px #031954,0 0 0 1px #1040c1",

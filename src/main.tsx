ef35c7f4b025db1d1923f43d48c31968503a0632
laska-adief <laskaadief.la@gmail.com>
2024-02-26 19:33:17 +0700

    [Paypal Clone] create router in main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import "./index.scss";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./pages/Login.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <App />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

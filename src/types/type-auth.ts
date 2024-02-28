8befe9600ebda6dbf98f0276bf91ac58ca82d7e0
laska-adief <laskaadief.la@gmail.com>
2024-02-28 21:04:53 +0700

    [Paypal Clone] create LoginFormProps in type-auth-

---
+++ src/types/type-auth.ts
@@ -0,0 +1,12 @@
+export interface LoginFormProps {
+  email: string;
+  password: string;
+  isShowPassword: boolean;
+  isFocusPassword: boolean;
+  onChangeEmail: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangePassword: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onShowHidePassword: (event: React.MouseEvent<HTMLButtonElement>, value: boolean) => void;
+  onFocusInputPassword: (event: React.FocusEvent<HTMLInputElement>) => void;
+  onBlurInputPassword: (event: React.FocusEvent<HTMLInputElement>) => void;
+  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+}

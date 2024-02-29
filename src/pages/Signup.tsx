b57b6ce55de42e058f36823fa4a35037e2aa9fc0
laska-adief <laskaadief.la@gmail.com>
2024-02-29 20:11:43 +0700

    [Paypal Clone] integrate activeStep state and function Signup with SignupForm

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import SignupForm from "../components/auth/SignupForm";
+import { SignupStepProps } from "../types/type-auth";
+
+const Signup = () => {
+  const signupStepInitial: SignupStepProps[] = [
+    { step: 1, label: "email", isCompleted: false },
+    { step: 2, label: "phone", isCompleted: false },
+  ];
+
+  const [steps, setSteps] = useState(signupStepInitial);
+  const [activeSteps, setActiveSteps] = useState(1);
+
+  const validStep = (stepNumber: number) => {
+    setSteps((prevStep) => {
+      return prevStep.map((step) => {
+        if (step.step === stepNumber) {
+          return { ...step, isCompleted: true };
+        }
+        return step;
+      });
+    });
+    setActiveSteps((prevStep) => prevStep + 1);
+  };
+  return <SignupForm steps={steps} validStep={validStep} activeStep={activeSteps} />;
+};
+export default Signup;

d5ac3542099a42267166bd070c2f86280bd8b053
laska-adief <laskaadief.la@gmail.com>
2024-03-07 18:51:33 +0700

    [Paypal Clone] add color and box shadow for error input

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,12 +12,14 @@ export default {
         separator: "#cbd2d6",
         "dark-gray": "#6c7378",
         error: "#545d68",
+        "input-border-error": "#d9360b",
       },
       padding: {
         form: "30px 40px 50px",
       },
       boxShadow: {
         "input-focus": "inset 0 0 0 .125rem #1040c1,0 0 0 .375rem rgba(16,114,235,.16),inset 0 0 0 1px #031954,0 0 0 1px #1040c1",
+        "input-focus-error": "inset 0 0 0 .0625rem #d9360b,0 0 0 .375rem rgba(217,54,11,.16),inset 0 0 0 1px #d9360b,0 0 0 1px #d9360b",
         "button-focus": "0 0 0 .375rem rgba(16,114,235,.16)",
         "button-secondary-hover": "inset 0 0 0 .0625rem #142c8e",
       },

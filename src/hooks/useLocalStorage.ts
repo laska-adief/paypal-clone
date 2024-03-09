14118334e4beabe7223a3ac0ec8dc114551275c6
laska-adief <laskaadief.la@gmail.com>
2024-03-09 16:41:50 +0700

    [Paypal Clone] create useLocalStorage hook setItem

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,8 @@
+const useLocalStorage = (key: string) => {
+  const setItem = (value: unknown) => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return { setItem };
+};
+export default useLocalStorage;

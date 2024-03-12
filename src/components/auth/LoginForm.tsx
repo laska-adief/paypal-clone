2f8eadda8640a8b94bf2cef74b4aaaeb3449d144
laska-adief <laskaadief.la@gmail.com>
2024-03-12 10:08:45 +0700

    [Paypal Clone] implement function onSubmit button in LoginForm

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -25,7 +25,7 @@ const LoginForm: React.FC<LoginFormProps> = ({
           <div className="max-w-[40px] mx-auto mb-10">
             <img src={PaypalLogo} alt="Paypal Logo" />
           </div>
-          <form onSubmit={onSubmit} className="flex flex-col gap-4 text-lg">
+          <div className="flex flex-col gap-4 text-lg">
             <div>
               <Input type="email" placeholder="Email" value={email} onChange={onChangeEmail} isError={emailError.length > 0} />
               {emailError?.length > 0 && (
@@ -73,7 +73,12 @@ const LoginForm: React.FC<LoginFormProps> = ({
             <Text isLink={true} href="/forgot-password" size="text-lg" weight="font-bold" customClass="mb-1">
               Forgot Password?
             </Text>
-            <Button type="submit" variant="primary" width="w-full" borderRadius="rounded-full">
+            <Button
+              type="submit"
+              variant="primary"
+              width="w-full"
+              borderRadius="rounded-full"
+              onClick={(event: React.MouseEvent<HTMLButtonElement>) => onSubmit(event)}>
               Log In
             </Button>
             <div className="relative h-4 text-center border-t border-separator mt-4 mb-2">
@@ -84,7 +89,7 @@ const LoginForm: React.FC<LoginFormProps> = ({
                 Sign Up
               </Button>
             </Link>
-          </form>
+          </div>
         </div>
       </div>
     </>

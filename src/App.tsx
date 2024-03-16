0f6d962949b7cef24ee9710e14b5923dbe8f06ba
laska-adief <laskaadief.la@gmail.com>
2024-03-16 10:45:51 +0700

    [Paypal Clone] navigate to either my account summary or login page based on user logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import useLocalStorage from "./hooks/useLocalStorage";
+import { useEffect, useState } from "react";
 function App() {
+  const { getItemStorage } = useLocalStorage("PaypalClone");
+  const authStorage = getItemStorage();
+  const authUser = authStorage?.loggedUser;
+  const navigate = useNavigate();
+
+  const [isNavigated, setIsNavigated] = useState(false);
+
+  useEffect(() => {
+    if (!isNavigated) {
+      if (authUser) {
+        navigate("/myaccount/summary");
+      } else {
+        navigate("/login");
+      }
+      setIsNavigated(true);
+    }
+  }, [authUser, navigate, isNavigated]);
+
   return (
     <>
       <Outlet />

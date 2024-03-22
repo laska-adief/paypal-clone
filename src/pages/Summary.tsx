ed43877e06c4557fca5255b4da6021a882a09e47
laska-adief <laskaadief.la@gmail.com>
2024-03-22 22:11:27 +0700

    [Paypal Clone] add Recent Activity section in summary page

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -7,13 +7,24 @@ const Summary = () => {
     <div>
       <Navbar />
       <div className="grid grid-cols-3 gap-x-8 max-w-screen-xl max-xl:px-4 p-8 px-14 m-auto max-md:grid-cols-1 max-md:gap-y-4 max-md:gap-x-0">
-        <div className="col-span-2 bg-white border border-black border-opacity-10 rounded-xl p-6 h-max">
-          <Text customClass="!text-lg !font-semibold mb-5">Paypal Balance</Text>
-          <Text customClass="!text-5xl !font-medium mb-2">4,15 $</Text>
-          <Text customClass="mb-8">Available</Text>
-          <Button type="button" variant="primary" customClass="rounded-full !px-4 !py-1">
-            Transfer Funds
-          </Button>
+        <div className="col-span-2 flex flex-col gap-8">
+          <div className="bg-white border border-black border-opacity-10 rounded-xl p-6 h-max">
+            <Text customClass="!text-lg !font-bold mb-5">Paypal Balance</Text>
+            <Text customClass="!text-5xl !font-medium mb-2">4,15 $</Text>
+            <Text customClass="mb-8">Available</Text>
+            <Button type="button" variant="primary" customClass="rounded-full !px-4 !py-1">
+              Transfer Funds
+            </Button>
+          </div>
+          <div className="bg-white border border-black border-opacity-10 rounded-xl p-6 h-max">
+            <Text customClass="!text-lg !font-bold mb-5">Recent Activity</Text>
+            <Text customClass="!text-lg mb-5">
+              See when money comes in, and when it goes out. You’ll find your recent PayPal activity here.
+            </Text>
+            <Text isLink={true} href="/myaccount/activities" customClass="!font-bold">
+              Show all
+            </Text>
+          </div>
         </div>
         <div className="bg-white border border-black border-opacity-10 rounded-xl p-6 h-max max-md:w-full">
           <div className="flex gap-2">

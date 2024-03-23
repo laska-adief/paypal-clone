418ee1f97c2a5473615562ca8a80b190857d12c2
laska-adief <laskaadief.la@gmail.com>
2024-03-23 08:35:45 +0700

    [Paypal Clone] add property custom class in Input component

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -6,12 +6,13 @@ interface InputProps {
   value: string;
   maxLength?: number;
   isError?: boolean;
+  customClass?: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
   onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
 }
 
-const Input: React.FC<InputProps> = ({ type, placeholder, value, maxLength, isError, onChange, onFocus, onBlur }) => {
+const Input: React.FC<InputProps> = ({ type, placeholder, value, maxLength, isError, customClass, onChange, onFocus, onBlur }) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -33,7 +34,7 @@ const Input: React.FC<InputProps> = ({ type, placeholder, value, maxLength, isEr
         onBlur={onBlur}
         className={`w-full px-3 py-5 border border-light-gray rounded focus:border-none focus:outline-dodger-blue focus:shadow-input-focus ${
           isError && "border-input-border-error focus:outline-0 focus:shadow-input-focus-error"
-        }`}
+        } ${customClass}`}
       />
     </>
   );

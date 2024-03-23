fbbc0d7f802ab7d956e214b5f6e80b3a777a91f8
laska-adief <laskaadief.la@gmail.com>
2024-03-23 08:36:28 +0700

    [Paypal Clone] create Activity page UI

--- src/pages/Activity.tsx
+++ src/pages/Activity.tsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+import Search from "../components/general/Search";
+
 const Activity = () => {
-  return <div>Activity</div>;
+  const [valueSearch, setValueSearch] = useState("");
+
+  const onChangeSearchValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target?.value;
+    setValueSearch(value);
+  };
+
+  return (
+    <div className="max-w-screen-xl max-xl:px-4 p-8 px-14 m-auto max-md:grid-cols-1 max-md:gap-y-4 max-md:gap-x-0">
+      <Search value={valueSearch} onChangeInputSearch={onChangeSearchValue} placeholder="Search by name or email" />
+    </div>
+  );
 };
 export default Activity;

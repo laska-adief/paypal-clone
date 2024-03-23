0c082f5592b73513c5278d7fee10f3937f05377f
laska-adief <laskaadief.la@gmail.com>
2024-03-23 09:00:20 +0700

    [Paypal Clone] add Filter By text in Activity page

--- src/pages/Activity.tsx
+++ src/pages/Activity.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Search from "../components/general/Search";
+import Text from "../components/common/Text";
 
 const Activity = () => {
   const [valueSearch, setValueSearch] = useState("");
@@ -12,6 +13,7 @@ const Activity = () => {
   return (
     <div className="max-w-screen-xl max-xl:px-4 p-8 px-14 m-auto max-md:grid-cols-1 max-md:gap-y-4 max-md:gap-x-0">
       <Search value={valueSearch} onChangeInputSearch={onChangeSearchValue} placeholder="Search by name or email" />
+      <Text customClass="text-lg pt-8 pb-4 font-semibold">Filter by</Text>
     </div>
   );
 };

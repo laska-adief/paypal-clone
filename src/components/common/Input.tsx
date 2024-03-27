6c72c60cf3d734a3a05e8ccd3051ee956a286767
laska-adief <laskaadief.la@gmail.com>
2024-03-27 19:24:04 +0700

    [Paypal Clone] update Input component to add id, name, and styling

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useRef } from "react";
 
 interface InputProps {
-  type: "text" | "email" | "password";
-  placeholder: string;
+  type: "text" | "email" | "password" | "radio";
+  placeholder?: string;
   value: string;
+  id?: string;
+  name?: string;
   maxLength?: number;
   isError?: boolean;
   customClass?: string;
@@ -12,8 +14,22 @@ interface InputProps {
   onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
 }
 
-const Input: React.FC<InputProps> = ({ type, placeholder, value, maxLength, isError, customClass, onChange, onFocus, onBlur }) => {
+const Input: React.FC<InputProps> = ({
+  type,
+  placeholder,
+  value,
+  id,
+  name,
+  maxLength,
+  isError,
+  customClass,
+  onChange,
+  onFocus,
+  onBlur,
+}) => {
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const isUsingFocus = type === "radio" ? false : true;
 
   useEffect(() => {
     if (isError && inputRef.current) {
@@ -27,14 +43,16 @@ const Input: React.FC<InputProps> = ({ type, placeholder, value, maxLength, isEr
         ref={inputRef}
         type={type}
         value={value}
+        id={id}
+        name={name}
         placeholder={placeholder}
         maxLength={maxLength}
         onChange={onChange}
         onFocus={onFocus}
         onBlur={onBlur}
-        className={`w-full px-3 py-5 border border-light-gray rounded focus:border-none focus:outline-dodger-blue focus:shadow-input-focus ${
-          isError && "border-input-border-error focus:outline-0 focus:shadow-input-focus-error"
-        } ${customClass}`}
+        className={`w-full px-3 py-5 border border-light-gray rounded focus:border-none focus:outline-dodger-blue ${
+          isUsingFocus && "focus:shadow-input-focus"
+        } ${isError && "border-input-border-error focus:outline-0 focus:shadow-input-focus-error"} ${customClass}`}
       />
     </>
   );

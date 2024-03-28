a0f95972dfcc8a85636acb0710708f39f1012e8a
laska-adief <laskaadief.la@gmail.com>
2024-03-28 20:00:00 +0700

    [Paypal Clone] add Filter Data Date

---
+++ src/static/FilterData.tsx
@@ -0,0 +1,22 @@
+export const FilterDate = [
+  {
+    label: "This Month",
+    value: "current-month",
+  },
+  {
+    label: "Last Month",
+    value: "last-month",
+  },
+  {
+    label: "Last 90 days",
+    value: "last-90-days",
+  },
+  {
+    label: "This Year",
+    value: "current-year",
+  },
+  {
+    label: "Last Year",
+    value: "last-year",
+  },
+];

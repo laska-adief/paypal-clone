d392c0f4cc5f00ec25da813f5b48d328434c54fb
laska-adief <laskaadief.la@gmail.com>
2024-03-30 08:25:10 +0700

    [Paypal Clone] add FilterButton in Activity page

--- src/pages/Activity.tsx
+++ src/pages/Activity.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Search from "../components/general/Search";
 import Text from "../components/common/Text";
+import FilterButton from "../components/general/FilterButton";
 import Container from "../components/general/Container";
 
 const Activity = () => {
@@ -16,6 +17,7 @@ const Activity = () => {
       <div className="max-w-screen-xl max-xl:px-4 p-8 px-14 m-auto max-md:grid-cols-1 max-md:gap-y-4 max-md:gap-x-0">
         <Search value={valueSearch} onChangeInputSearch={onChangeSearchValue} placeholder="Search by name or email" />
         <Text customClass="text-lg pt-8 pb-4 font-semibold">Filter by</Text>
+        <FilterButton id="filter-date" text="Date"></FilterButton>
       </div>
     </Container>
   );
